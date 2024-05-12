c14dcc66d6901934f2836d2b6f12ed12c8f9e005
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-11 21:58:24 -0300

    chore: add user base code

--- src/main/java/dev/sandros22/jblog/entities/User.java
+++ src/main/java/dev/sandros22/jblog/entities/User.java
@@ -1,4 +1,21 @@
 package dev.sandros22.jblog.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
+    private String name;
+    private String email;
+    private String password;
+    private Date birthday;
+    private Date lastLogin;
+    private Date created;
 }
