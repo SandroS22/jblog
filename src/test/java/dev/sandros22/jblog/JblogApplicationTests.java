28396f2f2786b7914271e1bc21334c136032a893
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-11 21:03:19 -0300

    initial commit

---
+++ src/test/java/dev/sandros22/jblog/JblogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sandros22.jblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
