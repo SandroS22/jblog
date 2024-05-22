680f833bc7a27844d00cec3607383ad67664b9a3
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-22 20:43:58 -0300

    chore: create post repository

---
+++ src/main/java/dev/sandros22/jblog/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package dev.sandros22.jblog.repositories;
+
+import dev.sandros22.jblog.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
