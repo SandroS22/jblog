6ce9f6bf0e2cf15be40665b3724092d9709076a8
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-22 20:44:30 -0300

    chore: create post service

---
+++ src/main/java/dev/sandros22/jblog/services/PostService.java
@@ -0,0 +1,39 @@
+package dev.sandros22.jblog.services;
+
+
+import dev.sandros22.jblog.entities.Post;
+import dev.sandros22.jblog.repositories.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public List<Post> findAll(){
+        return postRepository.findAll();
+    }
+
+    public Post findById(UUID id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
+    public void delete(UUID postId) {
+        postRepository.deleteById(postId);
+    }
+
+    public void update(Post post) {
+        postRepository.save(post);
+    }
+}
