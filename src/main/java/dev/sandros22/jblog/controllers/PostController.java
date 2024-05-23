187e027872820aad5671152912c4411c9734afe9
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-05-22 21:13:46 -0300

    feat: create post crud

--- src/main/java/dev/sandros22/jblog/controllers/PostController.java
+++ src/main/java/dev/sandros22/jblog/controllers/PostController.java
@@ -1,8 +1,62 @@
 package dev.sandros22.jblog.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import dev.sandros22.jblog.entities.Post;
+import dev.sandros22.jblog.services.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 @RestController("/post")
 public class PostController {
 
+    PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public List<Post> getPosts() {
+        return postService.findAll();
+    }
+
+    @GetMapping("/{postId}")
+    public Object getPost(@PathVariable UUID postId) throws HttpClientErrorException.NotFound {
+        Post post = postService.findById(postId);
+        return Objects.requireNonNullElseGet(post, () -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Post not found"));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createPost(@RequestBody Post post) {
+        postService.save(post);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> updatePost(@RequestBody Post post) {
+        Post oldPost = postService.findById(post.getPostID());
+        if (oldPost == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else {
+            oldPost.setContent(post.getContent());
+            postService.update(oldPost);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletePost(@PathVariable UUID postId) {
+        Post post = postService.findById(postId);
+        if (post == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else{
+            postService.delete(postId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+    }
 }
